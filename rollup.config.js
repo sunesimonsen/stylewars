0b77a8eb440f45b7a36e2c19ff67b0035ff031f8
Sune Simonsen <sune@we-knowhow.dk>
2020-12-05 15:31:40 +0100

    Provide minified builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,8 @@
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+
+const plugins = [commonjs(), nodeResolve()];
 
 export default [
   {
@@ -8,7 +11,7 @@ export default [
       file: "dist/bundle.cjs.js",
       format: "cjs",
     },
-    plugins: [commonjs(), nodeResolve()],
+    plugins,
   },
   {
     input: "src/index.js",
@@ -16,6 +19,22 @@ export default [
       file: "dist/bundle.esm.js",
       format: "esm",
     },
-    plugins: [commonjs(), nodeResolve()],
+    plugins,
+  },
+  {
+    input: "src/index.js",
+    output: {
+      file: "dist/bundle.cjs.min.js",
+      format: "cjs",
+    },
+    plugins: plugins.concat(terser()),
+  },
+  {
+    input: "src/index.js",
+    output: {
+      file: "dist/bundle.esm.min.js",
+      format: "esm",
+    },
+    plugins: plugins.concat(terser()),
   },
 ];
